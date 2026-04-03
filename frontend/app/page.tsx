a716192ae1482e71a1758051c09126753ae2a2ea
gayashan4lk <gayashan4lk@gmail.com>
2026-04-03 11:27:34 +0530

    add api base url env variable

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
 export default async function Home() {
-  const res = await fetch("http://localhost:8000");
+  const res = await fetch(`${process.env.API_BASE_URL}/`);
   const data = await res.json();
 
   return (
