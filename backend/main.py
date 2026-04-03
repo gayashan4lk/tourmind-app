9f20e41bdf0aa4f2a5044ee4a4577ceb73e76463
gayashan4lk <gayashan4lk@gmail.com>
2026-04-03 10:49:47 +0530

    create fastAPI app

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,20 @@
-def main():
-    print("Hello from backend!")
+# def main():
+#     print("Hello from backend!")
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
