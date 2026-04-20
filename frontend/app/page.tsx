7dfc99721d0b86ade6124001124dc7f2b231744c
gayashan4lk <gayashan4lk@gmail.com>
2026-04-20 13:53:40 +0530

    feat: add Prettier configuration and ignore files, implement Dashboard component, and update Home page with authentication session

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,19 +1,24 @@
-import Image from "next/image";
+import { auth } from '@/lib/auth'
+import { headers } from 'next/headers'
 
 export default async function Home() {
-  const res = await fetch(`${process.env.API_BASE_URL}/`);
-  const data = await res.json();
+	const res = await fetch(`${process.env.API_BASE_URL}/`)
+	const data = await res.json()
 
-  if (!data) {
-    return <div>API call failed</div>;
-  }
+	if (!data) {
+		return <div>API call failed</div>
+	}
 
-  return (
-    <div>
-      <h1 className="font-bold text-6xl">TourMind</h1>
-      <h1 className="text-2xl font-bold">System healthcheck</h1>
-      <p>Status: {data.status}</p>
-      <p>Service: {data.service}</p>
-    </div>
-  );
+	const session = await auth.api.getSession({
+		headers: await headers(),
+	})
+
+	return (
+		<div>
+			<h1 className="text-6xl font-bold">TourMind</h1>
+			<h1 className="text-2xl font-bold">System healthcheck</h1>
+			<p>Status: {data.status}</p>
+			<p>Service: {data.service}</p>
+		</div>
+	)
 }
